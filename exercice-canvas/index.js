668ce6218adf23af5671cd4b86bb616c8bdb6181
Milyra35 <marie.richir35@gmail.com>
2023-06-01 08:57:30 +0000

    js exercice canvas

---
+++ exercice-canvas/index.js
@@ -0,0 +1,127 @@
+function exercice6()
+{
+
+}
+
+function exercice5()
+{
+    let circleBlue = {
+        color:"blue",
+        radius:10,
+        x:150,
+        y:150
+    };
+    
+    let canvasDom = document.getElementById("ex5");
+    let ctx = canvasDom.getContext('2d');
+    
+    function grow() {
+        circleBlue.radius = circleBlue.radius + 10;
+    
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0,0, canvasDom.width, canvasDom.height);
+        ctx.fillStyle = circleBlue.color;
+        ctx.beginPath();
+        ctx.arc(circleBlue.x, circleBlue.y, circleBlue.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        
+        if (circleBlue.radius >= 135)
+        {
+            clearInterval(interval);
+        }
+    }
+    let interval = setInterval(grow, 100);
+}
+
+function exercice4()
+{
+    let circleBlue = {
+        color:"blue",
+        radius:100,
+        x:150,
+        y:100
+    };
+    
+    let canvasDom = document.getElementById("ex4");
+    let ctx = canvasDom.getContext('2d');
+    
+    
+    function moveCircle() {
+        circleBlue.x = circleBlue.x - 10;
+    
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0,0, canvasDom.width, canvasDom.height);
+        ctx.fillStyle = circleBlue.color;
+        ctx.beginPath();
+        ctx.arc(circleBlue.x, circleBlue.y, circleBlue.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        console.log(circleBlue.x);
+        
+        if (circleBlue.x <= 0)
+        {
+            clearInterval(interval);
+        }
+    }
+    let interval = setInterval(moveCircle, 100);
+}
+
+
+function exercice3()
+{
+    let canvasDom = document.getElementById("ex3");
+    let ctx = canvasDom.getContext('2d');
+    ctx.font = "bold 68px Montserrat";
+    ctx.fillStyle = "black";
+    ctx.fillText('Geometry.io', 10, 50);
+}
+
+function exercice2()
+{
+    let circle = {
+        color: "blue",
+        radius:100,
+        x:120,
+        y:120
+    };
+    
+    let canvasDom = document.getElementById("ex2");
+    let ctx = canvasDom.getContext('2d');
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0,0, canvasDom.width, canvasDom.height);
+    ctx.fillStyle = circle.color;
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function exercice1()
+{
+    let square = document.getElementById("ex1");
+    let ctx = square.getContext('2d');
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 200, 200);
+}
+
+function initCanvasSize()
+{
+    // setting the size of the canvas
+    let canvasList = document.querySelectorAll("canvas");
+
+    for(canvas of canvasList)
+    {
+        let section = canvas.parentNode;
+
+        canvas.width = section.clientWidth;
+        canvas.height = section.clientHeight;
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+   initCanvasSize();
+   exercice1();
+   exercice2();
+   exercice3();
+   exercice4();
+   exercice5();
+   exercice6();
+});
