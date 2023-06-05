066ff52e5cdaff3e1ec2cce0aa918d3d2e88a277
Milyra35 <marie.richir35@gmail.com>
2023-06-05 13:38:52 +0000

    Suite Arkanoid, ajout d'une barre sur laquelle la balle rebondit

--- arkanoid/index.js
+++ arkanoid/index.js
@@ -7,26 +7,26 @@ function playGame() {
     let ball = {
         color:"red",
         radius:10,
-        x:100,
-        y:100,
+        x:200,
+        y:200,
         directionX:0,
         directionY:-1,
     };
     
     let game = {
         color:"999",
-        width:200,
-        height:200
+        width:400,
+        height:400
     };
     
     let paddle = {
         color:"black",
-        x:75,
-        y:190,
-        width:50,
+        x:150,
+        y:390,
+        width:100,
         height:10,
-        speed:4,
-        direction:1
+        speed:6,
+        direction:0
     };
     
     let canvasDom = document.getElementById("canvas");
@@ -39,8 +39,8 @@ function playGame() {
         ctx.fillStyle = game.color;
         
         // The direction of the ball
-        ball.x += 1.5 * ball.directionX;
-        ball.y += 1.5 * ball.directionY;
+        ball.x += 2 * ball.directionX;
+        ball.y += 2 * ball.directionY;
         
         // Draw the ball
         ctx.fillStyle = ball.color;
@@ -61,7 +61,7 @@ function playGame() {
             {
                 ball.directionX = 1;
             }
-            else if (ball.x >= 190)
+            else if (ball.x >= 390)
             {
                 ball.directionX = -1;
             }
@@ -70,24 +70,43 @@ function playGame() {
             {
                 ball.directionY = ball.directionY * (-1);
                 
-                if (ball.x < paddle.x + (paddle.width / 2))
+                let relativePosition = (ball.x - paddle.x) / paddle.width;
+    
+                // Adjust the angle
+                if (relativePosition < 0.5 && relativePosition < 0.25) 
+                {
+                    ball.directionX = -2;
+                    ball.directionY = -2;
+                } 
+                else if (relativePosition > 0.5 && relativePosition > 0.75) 
+                {
+                    ball.directionX = 2;
+                    ball.directionY = -2;
+                }
+                else if (relativePosition < 0.5 && relativePosition > 0.25)
                 {
                     ball.directionX = -1;
                 }
-                else if (ball.x > paddle.x + (paddle.width / 2))
+                else if (relativePosition > 0.5 && relativePosition < 0.75)
                 {
                     ball.directionX = 1;
                 }
+                else 
+                {
+                    ball.directionX = 0;
+                }
             }
             // Condition if it falls
-            else if (ball.y > 200)
+            else if (ball.y > 400)
             {
                 gameIsOver = true;
-                if (gameIsOver === true)
+                if (gameIsOver)
                 {
-                    ctx.font = "bold 20px sans-serif";
+                    ctx.font = "bold 40px sans-serif";
                     ctx.fillStyle = "red";
-                    ctx.fillText("Game over !", 45, 100);
+                    ctx.fillText("Game over !", 75, 100);
+                    
+                    gameIsOver = false;
                     return;
                 }
             }
@@ -106,9 +125,10 @@ function playGame() {
         // console.log(event.code);
         if (event.code === "ArrowRight")
         {
+            paddle.direction =+ 1;
             paddle.x += paddle.speed * paddle.direction;
             
-            if (paddle.x >= 150)
+            if (paddle.x >= 400)
             {
                 paddle.x = 0;
             }
@@ -119,7 +139,7 @@ function playGame() {
             
             if (paddle.x <= 0)
             {
-                paddle.x = 200;
+                paddle.x = 400;
             }
         }
     });
@@ -146,4 +166,30 @@ function playGame() {
 //         }
         
 //     });
+// }
+
+
+
+
+// if (ball.x < paddle.x + (paddle.width / 4))
+// {
+//     ball.directionX = -1;
+//     ball.directionY = -2;
+// }
+// else if (ball.x < paddle.x + paddle.width / 4 && ball.x < 3 * (paddle.width / 4))
+// {
+//     ball.directionX = -2;
+// }
+// else if (ball.x > paddle.x + (paddle.width / 4))
+// {
+//     ball.directionX = 1;
+//     ball.directionY = 2;
+// }
+// else if (ball.x > paddle.x + (paddle.width / 4) && ball.x > 3 * (paddle.width / 4))
+// {
+//     ball.directionX = 2;
+// }
+// else 
+// {
+//     ball.directionX = 0;
 // }
